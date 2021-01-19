228163f1c38ee31b51eb10c985f2768ec910717a
MilicaCulibrk <milica6.1.97thebest@gmail.com>
2021-01-19 19:07:13 +0100

    added login for all roles

--- Library/src/main/resources/data.sql
+++ Library/src/main/resources/data.sql
@@ -3,3 +3,9 @@ insert into genre (genre_id, name) values(2, 'romance');
 insert into genre (genre_id, name) values(3, 'horror');
 insert into genre (genre_id, name) values(4, 'action');
 insert into genre (genre_id, name) values(5, 'drama');
+
+insert into editor (id, city, country, email, enabled, first_name, last_name, password, username)
+    values (112, 'Novi Sad', 'Serbia', 'mc@gmail.com', 'true', 'Milica', 'Culibrk', 'mc', 'mc');
+
+insert into book_prototype (book_status, plagiarism, synopsis, title, beta_reader_id, editor_id, genre_genre_id, lector_id, writer_id)
+    values (0, 'false', 'Book about justice and revenge.', 'The Count of Monte Cristo', null, 112, 5, null, null);
