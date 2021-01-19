d694361df6e3fa61af92d2c345eee3e38d4e6c16
Masa Matovic <masa.matovic97@gmail.com>
2021-01-19 19:24:21 +0100

    tasks for submit add book form

---
+++ Library/src/main/java/com/project/online_library/controller/CamundaController.java
@@ -0,0 +1,76 @@
+package com.project.online_library.controller;
+
+
+import com.project.online_library.dto.FormFieldsDto;
+import com.project.online_library.dto.FormSubmissionDto;
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.form.FormField;
+import org.camunda.bpm.engine.form.TaskFormData;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class CamundaController {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
+    FormService formService;
+
+    @GetMapping(path = "/getForm/{processId}", produces = "application/json")
+    public @ResponseBody
+    FormFieldsDto get(@PathVariable String processId) {
+
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey(processId);
+
+        Task task = taskService.createTaskQuery().processInstanceId(pi.getId()).list().get(0);
+        TaskFormData tfd = formService.getTaskFormData(task.getId());
+        List<FormField> properties = tfd.getFormFields();
+        for(FormField fp : properties) {
+            System.out.println(fp.getId() + fp.getType());
+        }
+
+        return new FormFieldsDto(task.getId(), pi.getId(), properties);
+    }
+
+    @PostMapping(path = "/subminForm/{taskId}/{variableName}", produces = "application/json")
+    public @ResponseBody
+    ResponseEntity crate(@RequestBody List<FormSubmissionDto> dto, @PathVariable String taskId,  @PathVariable String variableName) {
+        HashMap<String, Object> map = this.mapListToDto(dto);
+        System.out.println(map.toString());
+        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+        String processInstanceId = task.getProcessInstanceId();
+        runtimeService.setVariable(processInstanceId, variableName, dto);
+        formService.submitTaskForm(taskId, map);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    private HashMap<String, Object> mapListToDto(List<FormSubmissionDto> list)
+    {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        for(FormSubmissionDto temp : list){
+            System.out.println(temp.getId());
+            System.out.println(temp.getFieldValue());
+            map.put(temp.getId(), temp.getFieldValue());
+        }
+
+        return map;
+    }
+
+
+
+}
