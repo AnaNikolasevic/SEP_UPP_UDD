24ddacc1ae2484270f000ff09a22aab15e770337
Masa Matovic <masa.matovic97@gmail.com>
2021-01-19 22:36:41 +0100

    add random editor to book

---
+++ Library/src/main/java/com/project/online_library/camundaServices/GenerateEditorDelegate.java
@@ -0,0 +1,47 @@
+package com.project.online_library.camundaServices;
+
+import com.project.online_library.dto.FormSubmissionDto;
+import com.project.online_library.model.BookPrototype;
+import com.project.online_library.model.Editor;
+import com.project.online_library.repository.BookPrototypeRepository;
+import com.project.online_library.repository.EditorRepository;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class GenerateEditorDelegate implements JavaDelegate {
+
+    @Autowired
+    EditorRepository editorRepository;
+
+    @Autowired
+    BookPrototypeRepository bookPrototypeRepository;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        // random generisanje editora
+        List<Editor> editors = editorRepository.findAll();
+        Random rand = new Random(); //instance of random class
+        System.out.println(editors.size());
+        int int_random = rand.nextInt(editors.size());
+        Editor editor = editors.get(int_random);
+        System.out.println(editor.getId());
+
+        //dodeljivanje editora knjizi
+
+        List<FormSubmissionDto> form = (List<FormSubmissionDto>)delegateExecution.getVariable("form");
+        BookPrototype bookPrototype = bookPrototypeRepository.findByTitle(form.get(0).getFieldValue());
+        bookPrototype.setEditor(editor);
+        //setovanje editor variable u provesu
+        delegateExecution.setVariable("editor", editor);
+
+    }
+
+
+}
--- Library/src/main/resources/data.sql
+++ Library/src/main/resources/data.sql
@@ -6,6 +6,12 @@ insert into genre (genre_id, name) values(5, 'drama');
 
 insert into editor (id, city, country, email, enabled, first_name, last_name, password, username)
     values (112, 'Novi Sad', 'Serbia', 'mc@gmail.com', 'true', 'Milica', 'Culibrk', 'mc', 'mc');
+insert into editor ( id, city, country, email, enabled, first_name, last_name, password, username)
+    values (113, 'Novi Sad', 'Serbia', 'mm@gmail.com', 'true', 'Masa', 'Matovic', 'mm', 'mm');
+insert into editor ( id, city, country, email, enabled, first_name, last_name, password, username)
+    values (114, 'Novi Sad', 'Serbia', 'an@gmail.com', 'true', 'Ana', 'Nikolasevic', 'an', 'an');
+insert into editor ( id, city, country, email, enabled, first_name, last_name, password, username)
+    values (115, 'Novi Sad', 'Serbia', 'ss@gmail.com', 'true', 'Stanislava', 'Skoric', 'ss', 'ss');
 
 insert into book_prototype (book_status, plagiarism, synopsis, title, beta_reader_id, editor_id, genre_genre_id, lector_id, writer_id)
     values (0, 'false', 'Book about justice and revenge.', 'The Count of Monte Cristo', null, 112, 5, null, null);
